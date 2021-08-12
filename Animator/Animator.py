ab133608fe3d859ee0e034a8b9b1645bdebba5e7
KICU1129 <kisuke1129@gmail.com>
2021-08-12 19:40:16 +0900

    implement prerendering functions

--- Animator/Animator.py
+++ Animator/Animator.py
@@ -23,10 +23,14 @@ from Animator.tha.face_morpher import FaceMorpherSpec
 from Animator.tha.two_algo_face_rotator import TwoAlgoFaceRotatorSpec
 from Animator.util import rgba_to_numpy_image, extract_pytorch_image_from_filelike,process_image,show_img
 
+from Animator.GenerateFace import DataGenerator
+
 from msvcrt import getch
 
 import  database
 import time
+
+import pandas as pd
 
 
 class Animator:
@@ -46,15 +50,26 @@ class Animator:
         self.head_pose_solver = HeadPoseSolver()
 
         self.pose_size = len(self.poser.pose_parameters())
+        #use to generate image temps
         self.source_image = None
+        #use for save image temp
+        self.image_temp=None
+
         self.posed_image = None
         self.current_pose = None
         self.last_pose = None
 
+        #Create instance of DataGenerator
+        self.dataGenerator=DataGenerator(poser,torch_device)
+
         #self.image_path=os.path.dirname(os.path.abspath(__file__))+"/data/illust/image/face.png"
         self.image_path="./face.png"
+        
+        #for create source image
+        self.source_image = extract_pytorch_image_from_filelike(self.database.SettingImage).to(self.torch_device).unsqueeze(dim=0)
 
-        self.update_base_image()
+        # self.update_base_image()
+        self.read_image_temp()
 
         print("--- Ready OK   ---")
 
@@ -62,6 +77,13 @@ class Animator:
         #process_image(self.database.SettingImage)
         print("--- Update Base Image ---")
         self.source_image = extract_pytorch_image_from_filelike(self.database.SettingImage).to(self.torch_device).unsqueeze(dim=0)
+        self.dataGenerator.create_image()
+    
+    def change_base_image(self):
+        print("--- Change Base Image ---")
+        self.source_image = extract_pytorch_image_from_filelike(self.database.SettingImage).to(self.torch_device).unsqueeze(dim=0)
+        self.read_image_temp()
+
 
     def update_image(self):
         start=time.time()
@@ -83,12 +105,11 @@ class Animator:
             self.draw_face_landmarks(rgb_frame, face_landmarks)
             self.draw_face_box(rgb_frame, face_box_points)
 
-        resized_frame = cv2.flip(cv2.resize(rgb_frame, (192, 256)), 1)
-        pil_image = PIL.Image.fromarray(resized_frame, mode='RGB')
+        # resized_frame = cv2.flip(cv2.resize(rgb_frame, (192, 256)), 1)
+        # pil_image = PIL.Image.fromarray(resized_frame, mode='RGB')
 
-
-
-        if euler_angles is not None and self.source_image is not None:
+        if euler_angles is not None :
+            print("Estimate Faces")
             self.current_pose = torch.zeros(self.pose_size, device=self.torch_device)
             self.current_pose[0] = max(min(-euler_angles.item(0) / 15.0, 1.0), -1.0)
             self.current_pose[1] = max(min(-euler_angles.item(1) / 15.0, 1.0), -1.0)
@@ -114,19 +135,64 @@ class Animator:
             mouth_normalized_ratio = compute_mouth_normalized_ratio(face_landmarks, min_mouth_ratio, max_mouth_ratio)
             self.current_pose[5] = mouth_normalized_ratio
 
-            self.current_pose = self.current_pose.unsqueeze(dim=0)
+            print(f"current_pose={self.current_pose}")
 
-            posed_image = self.poser.pose(self.source_image, self.current_pose).detach().cpu()
-            numpy_image = rgba_to_numpy_image(posed_image[0])
+            # print(self.current_pose)
+
+            # self.current_pose = self.current_pose.unsqueeze(dim=0)
+
+            # posed_image = self.poser.pose(self.source_image, self.current_pose).detach().cpu()
+            numpy_image = self.approx_image()#rgba_to_numpy_image(posed_image[0])
             pil_image = PIL.Image.fromarray(np.uint8(np.rint(numpy_image * 255.0)), mode='RGBA')
 
-            self.database.SetAnimeFaces(np.array(numpy_image*255,dtype=np.uint8))
+            self.database.SetAnimeFaces(numpy_image)
 
             elapsed_time = time.time() - start
-            print(r"\rETA : {0} [sec]".format(elapsed_time) )
+            print(r"ETA : {0} [sec]".format(elapsed_time) )
 
             return pil_image,True
+        else:
+            return self.source_image,False
 
+    def approx_image(self):
+        start=time.time()
+
+        pose_index=["pose_0","pose_1","pose_2","pose_3","pose_4","pose_5"]
+        pose_diff=self.image_temp.copy()
+        pose_diff[[p+"_diff" for p in pose_index]]=np.abs(pose_diff[pose_index]-self.current_pose.cpu().numpy())
+
+        angles=[]
+        angles.append(pose_diff.sort_values(by=["pose_0_diff"], ascending=True).iloc[0].pose_0)
+        angles.append(pose_diff.sort_values(by=["pose_1_diff"], ascending=True).iloc[0].pose_1)
+        angles.append(pose_diff.sort_values(by=["pose_2_diff"], ascending=True).iloc[0].pose_2)
+        min_angle_idx=np.argmin(angles)
+
+        if angles[0]<0.1:
+            pose_diff=pose_diff[pose_diff[pose_index[0]]==angles[0]]
+        else:
+            pose_diff=pose_diff[pose_diff[pose_index[min_angle_idx]]==angles[min_angle_idx]]
+
+        # print(pose_diff)
+        parts=[]
+        base_img=pose_diff.iloc[0].image
+
+        parts.append(pose_diff.sort_values(by=["pose_3_diff"], ascending=True).iloc[0].image)
+        parts.append(pose_diff.sort_values(by=["pose_4_diff"], ascending=True).iloc[0].image)
+        parts.append(pose_diff.sort_values(by=["pose_5_diff"], ascending=True).iloc[0].image)
+
+        # elapsed_time = time.time() - start
+        # print("Image Sync ETA : {0} [sec]".format(elapsed_time) )
+
+        return self._sync_image(base_img,parts)
+
+    def _sync_image(self,base_img,parts):
+        sync_image=base_img
+
+        for part in parts:
+            diff_img=np.mean(base_img-part,axis=2)
+            sync_image[diff_img>3]=part[diff_img>3]
+
+        return sync_image
 
     def draw_face_box(self, frame, face_box_points):
         line_pairs = [[0, 1], [1, 2], [2, 3], [3, 0],
@@ -141,10 +207,14 @@ class Animator:
             x = part.x
             y = part.y
             cv2.rectangle(frame, (x - 1, y - 1), (x + 1, y + 1), (0, 255, 0), thickness=2)
+    
+    def read_image_temp(self):
+        self.image_temp=pd.read_pickle(self.database.fileManager.get_folder_path(self.database.BaseImageName)[1])
+        
 
 
 def CreateAnimator():
-    cuda = torch.device('cuda')
+    cuda = torch.device('cpu')
     poser = MorphRotateCombinePoser256Param6(
         morph_module_spec=FaceMorpherSpec(),
         morph_module_file_name="data/face_morpher.pt",
---
+++ Animator/GenerateFace.py
@@ -0,0 +1,261 @@
+import os
+import sys
+sys.path.append(os.getcwd())
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(os.path.join(os.path.dirname(__file__), '../..'))
+
+from tkinter import Tk, Frame, LEFT, Label, BOTH, GROOVE, Button, filedialog, PhotoImage, messagebox
+
+import numpy as np
+import PIL.Image
+import PIL.ImageTk
+import cv2
+import dlib
+import torch
+import pandas as pd
+
+from Animator.poser.morph_rotate_combine_poser import MorphRotateCombinePoser256Param6
+from Animator.puppet.head_pose_solver import HeadPoseSolver
+from Animator.poser.poser import Poser
+from Animator.puppet.util import compute_left_eye_normalized_ratio, compute_right_eye_normalized_ratio, \
+    compute_mouth_normalized_ratio
+from Animator.tha.combiner import CombinerSpec
+from Animator.tha.face_morpher import FaceMorpherSpec
+from Animator.tha.two_algo_face_rotator import TwoAlgoFaceRotatorSpec
+from Animator.util import rgba_to_numpy_image, extract_pytorch_image_from_filelike,process_image,show_img
+
+from msvcrt import getch
+
+import  database
+
+import pickle
+
+# from poser.morph_rotate_combine_poser import MorphRotateCombinePoser256Param6
+# from puppet.head_pose_solver import HeadPoseSolver
+# from poser.poser import Poser
+# from puppet.util import compute_left_eye_normalized_ratio, compute_right_eye_normalized_ratio, \
+#     compute_mouth_normalized_ratio
+# from tha.combiner import CombinerSpec
+# from tha.face_morpher import FaceMorpherSpec
+# from tha.two_algo_face_rotator import TwoAlgoFaceRotatorSpec
+# from util import rgba_to_numpy_image, extract_pytorch_image_from_filelike,process_image,show_img
+
+# from msvcrt import getch
+
+# import  database
+
+import time
+
+class ImageSaver:
+    def __init__(self,root="/temp/"):
+        self.image_idx=0
+        self.root=os.path.dirname(os.path.abspath(__file__))[:-9]+root
+        self.temp="anime_face_"
+        self.ext=".png"
+
+    def save(self,image):
+        path=self.root+self.temp+str(self.image_idx)+self.ext
+        cv2.imwrite(path, image)
+        self.image_idx+=1
+
+class ImageManager:
+    def __init__(self,data,root="/temp/"):
+        self.database=data
+
+        self.root=os.path.dirname(os.path.abspath(__file__))[:-9]+root
+        self.temp="anime_faces"
+        self.ext=".pkl"
+
+class DataGenerator:
+
+    def __init__(self,
+                 poser: Poser,
+                 torch_device: torch.device):
+        self.database=database.DataLoarder().create_database()
+        self.poser = poser
+        # self.video_capture = video_capture
+        self.torch_device = torch_device
+        self.head_pose_solver = HeadPoseSolver()
+
+        self.pose_size = len(self.poser.pose_parameters())
+        self.source_image = None
+        self.posed_image = None
+        self.current_pose = None
+        self.last_pose = None
+
+        #Create Anime Images database
+        self.images_temp=pd.DataFrame([],columns=["pose_0","pose_1","pose_2","pose_3","pose_4","pose_5","image"])
+        self.set_images_temp()
+
+        #self.image_path=os.path.dirname(os.path.abspath(__file__))+"/data/illust/image/face.png"
+        self.image_path="./face.png"
+
+        self.update_base_image()
+
+        print("--- Ready OK   ---")
+
+    def update_base_image(self):
+        #process_image(self.database.SettingImage)
+        print("--- Update Base Image ---")
+        self.source_image = extract_pytorch_image_from_filelike(self.database.SettingImage).to(self.torch_device).unsqueeze(dim=0)
+
+    def set_images_temp(self):
+        # start=time.time()
+        # frame = self.database.GetRealFaces()#self.video_capture.read()
+
+        # imageSaver=ImageSaver()
+        self.current_pose = torch.zeros(self.pose_size, device=self.torch_device)
+        
+        print("Start Generate Images")
+
+        euler_angles_list=np.linspace(-1,1,10)
+        eye_ratio_list=np.linspace(0,1,5)
+        mouth_ratio_list=np.linspace(0,1,5)
+
+        for eul_r in euler_angles_list:
+            for euler_idx in range(3):
+                _l=np.array([0,1,2])
+                _l=_l[~(_l==euler_idx)]
+                self.current_pose[euler_idx]=eul_r
+                self.current_pose[_l[0]]=1
+                self.current_pose[_l[1]]=1
+                
+                which__parts={
+                    "r_eye":eye_ratio_list,
+                    "l_eye":eye_ratio_list,
+                    "mouth":mouth_ratio_list
+                    }
+                
+                pose_idx=["pose_0","pose_1","pose_2","pose_3","pose_4","pose_5"]
+
+                for part_name,c_pose  in which__parts.items():
+                    if part_name=="r_eye":
+                        for i in c_pose:
+                            self.current_pose[3]=i
+                            self.current_pose[4]=0
+                            self.current_pose[5]=0
+                            _temp=pd.DataFrame({pose_idx[i] : [float(self.current_pose[i])]  for i in range(len(pose_idx))})
+                            self.images_temp=self.images_temp.append(pd.DataFrame(_temp))
+
+                            # numpy_image=self.create_anime_from_pose()
+                            # imageSaver.save(numpy_image)
+                
+                    if part_name=="l_eye":
+                        for i in c_pose:
+                            self.current_pose[3]=0
+                            self.current_pose[4]=i
+                            self.current_pose[5]=0
+                            _temp=pd.DataFrame({pose_idx[i] : [float(self.current_pose[i])]  for i in range(len(pose_idx))})
+                            self.images_temp=self.images_temp.append(pd.DataFrame(_temp))
+
+                            # numpy_image=self.create_anime_from_pose()
+                            # imageSaver.save(numpy_image)
+
+                    if part_name=="mouth":
+                        for i in c_pose:
+                            self.current_pose[3]=0
+                            self.current_pose[4]=0
+                            self.current_pose[5]=i
+                            _temp=pd.DataFrame({pose_idx[i] : [float(self.current_pose[i])]  for i in range(len(pose_idx))})
+                            self.images_temp=self.images_temp.append(pd.DataFrame(_temp))
+
+                            # numpy_image=self.create_anime_from_pose()
+                            # imageSaver.save(numpy_image)
+        self.images_temp.reset_index(inplace=True)
+        # print(self.images_temp)
+
+    def create_image(self):
+        start=time.time()
+
+        frame = self.database.GetRealFaces()#self.video_capture.read()
+        there_is_frame=len(frame)==0
+        euler_angles = None
+        face_landmarks = None
+
+        imageSaver=ImageSaver()
+        self.current_pose = torch.zeros(self.pose_size, device=self.torch_device)
+        
+        print("Start Generate Images")
+
+        euler_angles_list=np.linspace(-1,1,10)
+        eye_ratio_list=np.linspace(0,1,5)
+        mouth_ratio_list=np.linspace(0,1,5)
+
+        pose_idx=["pose_0","pose_1","pose_2","pose_3","pose_4","pose_5"]
+
+
+        for i in range(len(self.images_temp)):
+            #current_poseに値を突っ込む
+            for j in range(len(self.current_pose)):
+                self.current_pose[j]=self.images_temp.iloc[i][pose_idx].values[j]
+            numpy_image=self.create_anime_from_pose()
+            # print(self.images_temp.loc[[self.images_temp.index[i]],"image"])
+            self.images_temp.loc[[self.images_temp.index[i]],"image"]=[numpy_image]
+            # imageSaver.save(numpy_image)
+            
+        #save image and dataframe
+        self.save_data()
+
+        elapsed_time = time.time() - start
+        print(r"\rETA : {0} [sec]".format(elapsed_time) )
+
+        print("Finish the works!")
+
+        return True
+    
+    def save_data(self):
+        image_path,data_path=self.database.fileManager.get_new_path()
+        print(image_path)
+        print(data_path)
+        cv2.imwrite(image_path, cv2.imread(self.database.SettingImage))
+        self.images_temp.to_pickle(data_path)
+
+        #Reset Image Temp to reduce memory
+        self.images_temp=pd.DataFrame([],columns=["pose_0","pose_1","pose_2","pose_3","pose_4","pose_5","image"])
+
+
+    def create_anime_from_pose(self):
+        current_pose = self.current_pose.unsqueeze(dim=0)
+        posed_image = self.poser.pose(self.source_image, current_pose).detach().cpu()
+        numpy_image = rgba_to_numpy_image(posed_image[0])
+
+        return np.array(numpy_image*255,dtype=np.uint8)
+    
+
+
+    
+
+
+
+def CreateDataGenerator():
+    cuda = torch.device('cuda')
+    poser = MorphRotateCombinePoser256Param6(
+        morph_module_spec=FaceMorpherSpec(),
+        morph_module_file_name="data/face_morpher.pt",
+        rotate_module_spec=TwoAlgoFaceRotatorSpec(),
+        rotate_module_file_name="data/two_algo_face_rotator.pt",
+        combine_module_spec=CombinerSpec(),
+        combine_module_file_name="data/combiner.pt",
+        device=cuda)
+
+    # face_detector = dlib.get_frontal_face_detector()
+    # landmark_?locator = dlib.shape_predictor(os.path.dirname(os.path.abspath(__file__))+"\data\shape_predictor_68_face_landmarks.dat")
+
+    # video_capture = cv2.VideoCapture(0)
+
+    animator = DataGenerator(poser,  cuda)
+    return animator
+
+if __name__ == "__main__":
+    animator=CreateAnimator()
+    print("--- Ready OK !! ---")
+    animator.update_image().show()
+
+
+    # while True:
+    #     key = ord(getch())
+    #     if key==13:
+    #         animator.update_image().show()
+    #     if key==27:
+    #         break
+
